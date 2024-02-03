4f104591f853f537bcd95a24a0d068ae8f2ac45c
Jvzqz13 <j.vzqz13@gmail.com>
2024-02-02 20:46:19 -0500

    more styling

--- src/components/StarshipCard.js
+++ src/components/StarshipCard.js
@@ -12,14 +12,14 @@ function StarShipCard ({starships}) {
             <h1 className='starFont'> StarShips </h1>
            <div className='container'> 
             {starships.map((starships, index) => (
-               <Card bg='secondary' text='light' style={{width: "18rem"}} key={index} >
+               <Card className="bg-white p-2 text-dark bg-opacity-50"   bg='secondary' text='light' style={{width: "14rem"}} key={index} >
                <Card.Header> {starships.name}  </Card.Header>
                <Card.Body>
                  <Card.Title> { starships.model } </Card.Title>
                  <Card.Text>
                    Manufacturer: {starships.manufacturer}
                  </Card.Text>
-                 <Button variant="dark">Go somewhere</Button>
+                 <Button variant="dark"> More </Button>
                </Card.Body>
              </Card>
             ))}
